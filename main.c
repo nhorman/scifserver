ee2198a186e4c2fa3fe35f57b8ce24912c7b3b48
Neil Horman <nhorman@gmail.com>
2023-07-09 12:58:24 -0400

    Starting to implement main function

--- main.c
+++ main.c
@@ -2,5 +2,15 @@
 
 int main(int argc, char __unused *argv[])
 {
-	return 0;
+	int rc = 0;
+	LOGMSG(LOG_INFO, "Starting SCIF server\n");
+
+	if (config_components()) {
+		LOGMSG(LOG_ERR, "Failed to config subsystems, shutting down\n");
+		rc = 1;
+		goto out;
+	}
+
+out:
+	return rc;
 }
