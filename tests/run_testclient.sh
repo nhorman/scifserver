d852ee48f5bbb53641cc1cd37f1de4bc58855feb
Neil Horman <nhorman@gmail.com>
2023-07-18 17:02:05 -0400

    fixing up test case

--- tests/run_testclient.sh
+++ tests/run_testclient.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-set -x
+EXIT_CODE=0
 
 ../scifserver/scifserver -k ./www.example.com.key -c ./www.example.com.cert > ./scifserver.log 2>&1 &
 
 sleep 2
 
 ../testclient/scifclient > ./scifclient.log 2>&1
+if [ $? -ne 0 ]
+then
+	echo "test client failed"
+	EXIT_CODE=127
+fi
+
+sleep 2
+
+kill -SIGKILL $(pidof scifserver)
 
 
-kill -SIGINT $(pidof scifserver)
+echo "SCIF SERVER LOGS"
+echo ========================================
+cat ./scifserver.log
+
+echo "SCIF CLIENT LOGS"
+echo ========================================
+cat ./scifclient.log
+
+exit $EXIT_CODE
 
 
